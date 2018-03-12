925e006ac362014c7711c88312ee7154fb5cdf37
nemanja888 <nemanja.jekic90@gmail.com>
2018-03-12 15:49:28 +0100

    commit license

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,3 +1,35 @@
-/**
- * Created by alien1 on 3/12/2018.
- */
+/*
+* name: LinkIt
+* autor: Nemanja
+* version: 0.1.0
+* license: MIT
+* */
+
+(function($){
+    $.fn.linkIt = function(options){
+        //default settings
+        var settings = $.extend({
+            href    :null,
+            text    :null,
+            target  : '_self'
+        }, options);
+
+        //validation
+        if(settings.href == null){
+            console.log('You need an href option for LinkIt to work');
+            return this;
+        }
+
+        return this.each(function(){
+            var object = $(this);
+
+            if(settings.text == null){
+                settings.text = object.text();
+            }
+
+            object.wrap('<a target="' + settings.target + '" href="' + settings.href + '"></a>').text(settings.text);
+        });
+
+
+    }
+}(jQuery));
